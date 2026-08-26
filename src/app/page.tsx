9c8820194f89c3f861d2a949f90ea2d829dde878
filipesilvagalvao <felepesilvagalvao@gmail.com>
2026-08-26 12:39:03 -0300

    estilização do componente de tarefas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client"
 import NovaTarefa from "@/components/nova_tarefa/NovaTarefa";
 import Task from "@/components/task/Task";
 
 export default function Home() {
+  const dados = JSON.parse(localStorage.getItem("tasks"))
   return (
-   <main>
-     <NovaTarefa/>
-     <Task/>
-   </main>
+    <main>
+      <NovaTarefa />
+      {
+        dados?.map((task) => (
+          <Task
+            text={task.text}
+            status={task.status}
+            key={task.id}
+          />
+        ))
+      }
+    </main>
   );
 }
--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,19 +1,30 @@
+"use client"
 import styles from "./Task.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
-function Task() {
+type Task_props = {
+    text:string,
+    status:boolean,
+    id:number
+}
+
+function Task({text, status, id}:Task_props) {
     return (
-        <article>
-            <div>
-                <input type="checkbox" name="" id="" />
+        <article className={styles.task}>
+
+            <div className={styles.task__check}>
+                <input type="checkbox" name="" id="" checked={status}/>
             </div>
-            <p></p>
-            <div>
+
+            <p className={styles.task__text}>{text}</p>
+
+            <div className={styles.task__delete}>
                 <button>
                     <FontAwesomeIcon icon={faTrash} />
                 </button>
             </div>
+
         </article>
     )
 }
