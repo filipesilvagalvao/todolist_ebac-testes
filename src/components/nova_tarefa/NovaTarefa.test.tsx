db028a4cc965b560f51a994d347548f5b143405a
filipesilvagalvao <felepesilvagalvao@gmail.com>
2026-09-03 14:58:36 -0300

    estruturando teste unitários

---
+++ src/components/nova_tarefa/NovaTarefa.test.tsx
@@ -0,0 +1,10 @@
+import {render} from "@testing-library/react";
+import NovaTarefa from "./NovaTarefa";
+
+test("Submete uma nova tarefa", ()=>{
+    const nova_tarefa = "Tarefa de teste";
+
+    render(<NovaTarefa/>)
+
+    
+})
